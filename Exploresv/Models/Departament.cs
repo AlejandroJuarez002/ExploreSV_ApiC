63f505069f1bcb7c2370c833e7ca40bcda4cd4fd
DanielMasin <164657442+DanielMasin@users.noreply.github.com>
2025-09-22 15:29:57 -0600

    Initial Commit

---
+++ Exploresv/Models/Departament.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exploresv.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        [MaxLength(30)]
+        public string Name { get; set; }= default!;
+    }
+}
